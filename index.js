1474c096aa8b89b4722c28514e135ce43c10a9c3
Pramitha Samarakoon <PramithaS@99x.lk>
2018-04-08 21:03:25 +0530

    willmonunt

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Text, AppRegistry} from 'react-native';
+import {Text,View, AppRegistry} from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
 const App = ()=>{
   return(
-
+    <View>
     <Header headerText={'Music Albums'}/>
-    //<AlbumList/>
+    <AlbumList/>
+    </View>
   );
 
 };
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { View, Text} from 'react-native';
 
-const AlbumList=()=>{
-  return (
-    <View>
-      <Text>AlbumList!!</Text>
-    </View>
-  );
-};
+//const AlbumList=()=>{
+class AlbumList extends Component{
+  componentWillMount(){
+    console.log('componentWillMount in here');
+
+  }
+  render(){
+    return (
+      <View>
+        <Text>AlbumList!!</Text>
+      </View>
+    );
+  }
+  }
 
 export default AlbumList;
